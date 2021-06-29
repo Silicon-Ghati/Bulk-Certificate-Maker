30d3c4a042fc25293052c2ce3a6f87353b769a59
AyushR1 <ayushrathore12501@gmail.com>
2021-06-29 12:24:27 +0530

    Add automatic emailling support
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- certs.py
+++ certs.py
@@ -1,6 +1,8 @@
 #Imports Required Packages from PIL
 from PIL import Image, ImageDraw, ImageFont
 
+from yagmail import SMTP, inline
+
 #Import Pandas for better access of Data and .xlsx File
 import pandas as pd
 
@@ -9,13 +11,20 @@ data = pd.read_excel("file.xlsx")
 
 #Import 'Name' List from the imported .xlsx file
 name_list = data['Name'].to_list()
+Email_list = data['Email'].to_list()
+LOGO='logo.png'
+
 
 #Determining the length of the list
 max_no = len(name_list)
 
+SENDER_EMAIL = ''  # SET THIS
+SENDER_EMAIL_PASSWORD = ''  # SET THIS
+idpass = SMTP(SENDER_EMAIL, SENDER_EMAIL_PASSWORD)
+
 #The Loops for creating certificates in bulk
 # for i in name_list:
-for idx, i in enumerate(name_list):
+for i, j in zip(name_list, Email_list):
 
     im = Image.open("cert.jpg")
     d = ImageDraw.Draw(im)
@@ -23,11 +32,20 @@ for idx, i in enumerate(name_list):
     text_color = (0, 137, 209)
     font = ImageFont.truetype("AlexBrush-Regular.ttf", 250, encoding="unic")
     d.text(location, i.title(), fill=text_color,font=font)
+
+    
     im.save("out/certificate_"+i+".pdf")
-    print("(%d/%d) Certificate Created for:  %s" % (idx+1, max_no, i.title()))
+    print("Certificate Created for:  %s" % (i.title()))
+    mailtext = 'Dear %s,\n\n. Enter your text for the automatically generated emai\n\n' % (i)
+    title = 'Enter your title'
+    email = j.title()
+    contents = [mailtext, inline(LOGO)]
+    print("Emailing this %s to %s" % (i.title(), j.title()))
+    idpass.send(email, title,
+        contents, attachments="out/certificate_"+i+".pdf")
     
 print("""\n*************************
-All Certificates Created! \nPlease follow me on Github for more awesome codes\n\tGitHub: https://github.com/mursalfk
+All Certificates Created and mailed to respective emails.
 *************************
 """)
 #Read readme.md for further instructions
